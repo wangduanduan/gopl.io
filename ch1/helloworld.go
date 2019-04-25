77cfc9efb0a3988b2d2da367ac0559655f9d09f8
wangduanduan <1779706607@qq.com>
2019-04-25 13:55:37 +0800

    00

---
+++ ch1/helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println('你好，世界')
+	// go 语言中，字符串必须要用双引号
+	fmt.Println("你好，世界")
+}
+
+// go run helloworld.go 编译完就运行
+// go build helloworld.go 仅仅编译成可执行文件
